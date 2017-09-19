6009d68e3b27447644c0f0c19ec60699e222f1d0
leahliya <leahliya@gmail.com>
2017-09-18 20:53:42 -0500

    not finished practice

--- practice.js
+++ practice.js
@@ -5,21 +5,24 @@
 
 
 // Rewrite the following line so that inner is scoped to the if statement.
-if ( true ) {
-	var inner = "Inner!";
+if (true) {
+	let inner = "Inner!";
 }
 
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
+const noChange = 100;
 
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
+const vocab = ["let", "var", "destructuring", "spread", "rest"];
 
 
 // change "var" to "const" at index 1 of vocab
+vocab[1] = "const";
 
 
 // add "arrow function" to the end of vocab
-
+vocab.push("arrow function");
 
 
 ///////////////////////
@@ -27,11 +30,12 @@ if ( true ) {
 //////////////////////
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
-
+const first = "Leah";
+const last = "Dombrovsky";
 
 
 // Declare the variable fullName and set it equal to your full name using template literals
-
+let fullName = `${first} ${last}`;
 
 
 //////////////////////
@@ -42,12 +46,22 @@ if ( true ) {
 // first and last should use object literal shorthand (with the variables created above),
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
-
+let me = {
+	first,
+	last,
+	lovesDevMountain: true,
+	learnES6() {
+      return "This is awesome!"
+	} 
+}
 
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
-
+let flipped = {
+	[first]: 'first',
+	[last]: 'last'
+};
 
 //////////////////////////////
 /* ASSIGNMENT DESTRUCTURING */
@@ -62,6 +76,7 @@ const destructuring = {
 
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
+let {objects, arrays} = destructuring;
 
 
 
@@ -71,7 +86,7 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
-
+let [notVar, alsoNotVar, ...everythingElse]=vocabAgain;
 
 
 ////////////
@@ -84,6 +99,7 @@ const lastFive = [ 6, 7, 8, 9, 10 ];
 
 // using the spread operator, create a new array named oneThroughTen.
 // You should use the above arrays (and 2 missing digits of your own)
+let oneThroughTen = [...firstThree, 4, 5, ...lastFive];
 
 
 // do not modify
@@ -99,6 +115,7 @@ function whatToUse( past, present, future ) {
 // using the spread operator, pass each element of jsVersions as an individual argument to whatToUse
 // and save the return value in a variable named currentJS.
 
+let currentJS = whatToUse(...jsVersions);
 
 
 ///////////////
@@ -106,26 +123,42 @@ function whatToUse( past, present, future ) {
 ///////////////
 
 // modify greeter to greet Anonymous if no name is passed.
-function greeter( name ) {
+function greeter( name = "Anonymous") {
 	return `Hi ${ name }!`
 }
+
 
 
 // write a function named toPower that takes two arguments, a number and an exponent,
 // and returns number to the power of exponent.
 // if no exponent is passed, the exponent should be two
 
+function toPower(num, exponent = 2){
+	return num**exponent
+}
+
 
 // using rest parameters, write a function named evenOdd that takes in
 // any number of parameters and returns an object with two properties - even and odd.
 // this function should check each parameter and push it to the appropriate property.
-
+// function evenOdd(...rest){
+// 	let obj={
+// 		even: [],
+// 		odd: []
+// 	}
+// 	if (rest[i]%2===0){
+// 		obj.even.push(rest[i]);
+// 	} else {
+// 		obj.odd.push(rest[i]);
+// 	}
+// 	return obj;
+//   }
 
 
 // write a function named multiply that takes in a num parameter and an arbitrary amount of
 // additional numbers. This function should return an array of each additional number
 // multiplied by num.
-
+// function multiply(num, amit)
 
 
 // do not modify
@@ -134,6 +167,8 @@ const bits = [ 2, 4, 8, 16, 32, 64, 128 ];
 // using an arrow function and the built in .map method, create a new array
 // named mooresBits. mooresBits should be the bits array doubled.
 
+// let mooresBits => map.bits*2;
+		
 
 // do not modify
 const that = {
